6ac42910003952f350e5cbb0f6681514d0170b1f
RebeccaJ21 <rebecca.prows@gmail.com>
2018-01-15 16:49:26 -0800

    random cust per day done

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',' 8pm'];
-
+//15 hours
 var storeName = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
 var pike = {
-
   minCust: 23,
   maxCust: 65,
-  avgCust: 6.3,
+  avgCookies: 6.3,
   render: function() {
     var ulEl = document.getElementById('firstandpike');
+    var randomNumDay = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+    console.log('before round', randomNumDay);
+    randomNumDay = Math.round(randomNumDay);
+    console.log('after round', randomNumDay);
 
-    for(var i = 0; i < this.hours.length; i++) {
+    var randomHour = Math.random() * (this.randomNumDay / this.avgCookies);
+    console.log('before round', randomHour);
+    randomHour = Math.round(randomHour);
+    console.log('after round', randomHour);
+
+
+    for(var i = 0; i < this.hours; i++) {
       var liEl = document.createElement('li');
-      liEl.textContent = storeName[0] + ': ' + this.hours[i] + hours;
+      liEl.textContent = storeName[i] + ': ' + this.hours[i] + hours;
       ulEl.appendChild(liEl);
     }
   }
